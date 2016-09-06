c3f5f9939fdfcf1f014e6045e005e31cc1ec8e1a
Alex Saber <Alex Saber>
2016-09-06 06:38:39 -0500

    added department labels to products

---
+++ seasonal-sales.js
@@ -0,0 +1,46 @@
+//products, the name of the department it's in, and the price
+var container = document.getElementById("container");
+
+var products;
+function getProducts () {
+  var data = JSON.parse(this.responseText);
+  products = data.products;
+}
+
+var categories;
+function getCategories () {
+  var data = JSON.parse(this.responseText);
+  categories = data.categories;
+  domOutput();
+}
+
+function domOutput() {
+  categories.forEach(function(object) {
+    for (var i = 0; i < products.length; i++) {
+      if (object.id === products[i].category_id) {
+        container.innerHTML += "<div>" + object.name + " - " + products[i].name + " " + products[i].price + "</div>";
+      }
+    }
+  })
+}
+
+function executeThisCodeIfXHRFails () {
+  console.log("An error occurred while transferring");
+}
+
+function executeThisCodeWhenChunksArrive () {
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", getProducts);
+myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+myRequest.addEventListener("progress", executeThisCodeWhenChunksArrive);
+myRequest.open("GET", "products.json");
+myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", getCategories);
+myRequest2.addEventListener("error", executeThisCodeIfXHRFails);
+myRequest2.addEventListener("progress", executeThisCodeWhenChunksArrive);
+myRequest2.open("GET", "categories.json");
+myRequest2.send();
